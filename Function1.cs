7df49164156007c97d65eb04e74fb53fa5d3b893
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2023-02-27 22:38:53 +0530

    Added Dependency injection,

--- Function1.cs
+++ Function1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using ImageRezierFunction.Interface;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -9,10 +10,29 @@ namespace ImageRezierFunction
     [StorageAccount("BlobConnectionString")]
     public class Function1
     {
+        private readonly IImageResizer imageResizer;
+
+        public Function1(IImageResizer imageResizer) => this.imageResizer = imageResizer;
+
         [FunctionName("Function1")]
-        public void Run([BlobTrigger("normal-size/{name}")]Stream myBlob, string name, ILogger log)
+        public void Run([BlobTrigger("normal-size/{name}")]Stream inputBlob,
+
+            [Blob("reduced-size/{name}", FileAccess.Write)] Stream outputBlob,
+            
+            string name, ILogger log)
         {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {inputBlob.Length} Bytes");
+
+            try
+            {
+                this.imageResizer.Resize(inputBlob,outputBlob);
+
+                log.LogInformation("Redeced image is saved to blob storage");
+            }
+            catch (Exception ex) {
+
+                log.LogError("Resize error", ex);
+            }
         }
     }
 }
