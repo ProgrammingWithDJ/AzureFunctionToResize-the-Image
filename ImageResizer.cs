0f8c7041fbe3d8767f2088ea8301304963f2a576
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2023-02-27 22:01:11 +0530

    Add project files.

---
+++ ImageResizer.cs
@@ -0,0 +1,25 @@
+﻿using ImageRezierFunction.Interface;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Formats.Jpeg;
+
+namespace ImageRezierFunction
+{
+    public class ImageResizer : IImageResizer
+    {
+        public void Resize(Stream input, Stream output)
+        {
+            using(Image image = Image.Load(input))
+            {
+                image.Mutate(x =>x.Resize(image.Width/2, image.Height/2));
+                image.Save(output, new JpegEncoder());
+            }
+        }
+    }
+}
---
+++ Interface/IImageResizer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageRezierFunction.Interface
+{
+    public interface IImageResizer
+    {
+        void Resize(Stream input,Stream output);
+    }
+}
